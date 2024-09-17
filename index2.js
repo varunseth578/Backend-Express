cacf616ffed0269c4f0ff9cd035efb8fd99cd39a
Varun Seth <122156664+varunseth578@users.noreply.github.com>
2024-09-17 10:57:32 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,3 +1,22 @@
 //http is use to expose functionality to world
 // by using express and spring bot in java http is protocol (node.js)
 // creating http server 
+const express = require("express");
+
+const app = express();
+
+function calsum(n) {
+  let ans = 0;
+  for (let i = 0; i <= n; i++) {
+    ans = ans + i;
+  }
+  return ans;
+}
+app.get("/", function (req, res) {
+    const n= req.query.n;
+    const ans = calsum(n);
+  res.send("hii there"+ans);
+});
+
+app.listen(3000);
+
